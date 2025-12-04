8dcee13a5ca230905661ef3e68feca25eca95d39
DHANUSH-mgr <isgreatlove1@gmail.com>
2025-12-04 01:29:45 -0800

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,18 @@
+package day1;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int rollnumber=201;
+char logo='t';
+String nameofthestudent="Namitha";
+boolean value=true;
+System.out.println(rollnumber);
+System.out.println(logo);
+System.out.println(nameofthestudent);
+System.out.println(value);
+
+	}
+
+}
